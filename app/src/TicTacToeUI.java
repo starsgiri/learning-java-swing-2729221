3fc520333fd69144e1c2241760bcfa88372e7d85
starsgiri <giriofkala2004@gmail.com>
2026-09-01 22:28:18 +0530

    learned the frame => panel => component

--- app/src/TicTacToeUI.java
+++ app/src/TicTacToeUI.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 
 public class TicTacToeUI extends JFrame{
     private JPanel contentPanel;
+    private JPanel C1R1;
+    private JPanel C1R2;
+    private JPanel C1R3;
+    private JPanel C2R1;
+
     public TicTacToeUI() {
         setTitle("Let's Play Tic Tac Toe");
         setSize(400,400);
